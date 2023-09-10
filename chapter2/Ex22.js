b9208630c1088f36a8f574a341d8874899ee7503
prxcbf-le <robinwen024@gmail.com>
2023-09-10 20:20:23 +0800

    very slow one step chess,hhhh

--- chapter2/Ex22.js
+++ chapter2/Ex22.js
@@ -1,59 +1,128 @@
 class TurtleGraph {
     constructor(CanvasId, VSHADER_SOURCE, FSHADER_SOURCE) {
-        let canvasN = document.getElementById(CanvasId);
-        this.canvas=canvasN;
-        this.clickCallback=(event)=>{
-            let mx = event.clientX; // 相对于document的位置
-            let my = event.clientY;
+        const canvasN = document.getElementById(CanvasId);
+        this.canvas = canvasN;
+        this.clickCallback = (event) => {
+            const mx = event.clientX; // 相对于document的位置
+            const my = event.clientY;
             console.log("相对于可视document x=" + mx + ", y=" + my);
-    
-            // let rect = this.canvas.getBoundingClientRect(); // 这里无需再考虑margin
-            // let dx = mx - rect.left;
-            // let dy = my - rect.top;
-            // console.log("canvas x=" + dx + ", y=" + dy);
-    
-            // this.textview.innerHTML += "<br/>canvas点击位置 x=" + dx + ", y=" + dy;
-            // this.textview.scrollTop = this.textview.scrollHeight;
-            //7*vx2+b=1.33 n*vx2+1.33-7*vx2=1-y n=(7*vx2-1.33)/vx2
-            let findGrid=(x,y)=>
-            {
-               // x=x*2;
+
+
+            //
+            const ifInGrid = (x, y) => {
+                if (x < 1 || x >= 18 || y < 1 || y > 13) {
+                    return false;
+                }
+                const mid = 7;
+                if ((x > 4 && x < 9) || x >= 14) {
+                    x = 18 - x;
+                }
+                const lefty = mid - Math.floor((x + 2) / 2) + 1;
+                const righty = mid + Math.floor((x + 1) / 2) - 1;
+                if (y < lefty || y > righty) {
+                    return false;
+                }
+                return true;
+            }
+            const findGrid = (x, y) => {
+                // x=x*2;
                 //y=y*2;
-                let radius = 0.03;
-                let vx2=0.15;
-                let vx3=0.05;
-                let nx=Math.round((-y-1.63)/vx2+9);
-                let ny1=Math.round((x-0.16)/vx3+6);
-                let ny2=Math.round((x-0.16)/vx3+6.5);
-                let ny=(nx%2==1)? ny2:ny1;
-                console.log("ny="+ny+"nx="+nx);
-                return [nx,ny];
+                const radius = 0.03;
+                const vx2 = 0.15;
+                const vx3 = 0.05;
+                const nx = Math.round((-y - 1.63) / vx2 + 9);
+                const ny1 = Math.floor((x - 0.16) / vx3 + 7.25);
+                const ny2 = Math.floor((x - 0.16) / vx3 + 7.5);
+                const ny = (nx % 2 == 1) ? ny2 : ny1;
+                console.log("ny=" + ny + "nx=" + nx, "ifInGrid=" + ifInGrid(nx, ny));
+                return [nx, ny];
             };
-            const rect=event.target.getBoundingClientRect();
-            let x = ((mx-rect.left)-this.canvas.width/2)/(this.canvas.width);
-            let y =  (this.canvas.height/2-(my-rect.top))/(this.canvas.height/2);
-            console.log("canvas x=" + x + ", y=" + y);
-            let [nx,ny]=findGrid(x,y);
-            let vt=Array.from(this.arr2d);
-            vt[nx][ny]=3;
-            this.drawCheckBoard(vt);
+            //player 1  player 2
+            const playerStep = (nx,ny,playerNum) => {
+                //2 2win 1 win 0 contiue
+                let array3d=this.arr2d;
+                if(array3d[nx][ny]!==playerNum){
+                    return 0;
+                }
+                const checkWin = () => {
+                    let play2Win = true, play1Win = true;
+                    let OnePlayerNum = 1;
+                    let twoPlayerNum = 2;
+                    for (let f = 1; f < 5; f += 1) {
+                        for (let v = 0; v < Math.floor((f + 1) / 2); v++) {
+                            if (array3d[f][mid + v] !== twoPlayerNum) {
+                                play2Win = false;
+                                break;
+                            }
+                        }
+                        for (let t = 1; t < Math.floor((f + 2) / 2); t++) {
+                            if (array3d[f][mid - t] !== twoPlayerNum) {
+                                play2Win = false;
+                                break;
+                            }
+                        }
+                    }
+                    if (play2Win) {
+                        console.log("player2Win!!");
+                        return 2;
+                    }
+                    for (let f = 14; f < 18; f += 1) {
+                        let f1 = 18 - f;
+                        for (let v = 0; v < Math.floor((f1 + 1) / 2); v++) {
+                            if(array3d[f][mid + v] !== OnePlayerNum){
+                                play1Win=false;
+                                break;
+                            }
+                        }
+                        for (let t = 1; t < Math.floor((f1 + 2) / 2); t++) {
+                            if(array3d[f][mid - t] !== OnePlayerNum){
+                                play1Win=false;
+                                break;
+                            }
+                            
+                        }
+                    }
+                    if (play1Win) {
+                        console.log("player1Win!");
+                        return 1;
+                    }
+                    return 0;
+                };
+                //start perpare to run
+                const pr=(nx%2==1)?[[-1,0],[1,0],[0,1],[0,-1],[-1,-1],[1,-1]]:
+                                   [[-1,0],[1,0],[0,1],[0,-1],[-1,1],[1,1]];
+                for(let i=0;i<6;i++)
+                {
+                    const mx=pr[i][0]+nx;
+                    const my=pr[i][1]+ny;
+                    if(ifInGrid(mx,my)&&array3d[mx][my]===0)
+                    {
+                        array3d[mx][my]=3;
+                    }
+                }
+                this.drawCheckBoard(array3d);
+            };
+            const rect = event.target.getBoundingClientRect();
+            const x = ((mx - rect.left) - this.canvas.width / 2) / (this.canvas.width);
+            const y = (this.canvas.height / 2 - (my - rect.top)) / (this.canvas.height / 2);
+            //console.log("canvas x=" + x + ", y=" + y);
+            const [nx, ny] = findGrid(x, y);
+            playerStep(nx,ny,1);
         };
-        let thet=this;
-        this.drawCheckBoard=(array2d)=>{
+        const thet = this;
+        this.drawCheckBoard = (array2d) => {
             if (array2d.length != 20) {
                 throw Error('array2d.length!=20');
             }
             if (array2d[0].length != 15) {
                 throw Error('array2d[0].length!=15');
             }
-            thet.arr2d =Array.from(array2d);
+            thet.arr2d = array2d;
             const gl = this.gl;
-            let pointArr = [];
-            let eageLen = 1.0 / 15;
-            let eageWidth = 1.0 / 20;
-            let pi = this.pi;
-            let that=thet;
-            let drawEage = () => {
+            const eageLen = 1.0 / 15;
+            const pi = this.pi;
+            const that = thet;
+            const drawEage = () => {
                 that.init(0, 0, this.pi / 2);
                 that.forward(eageLen * 15 / 3 * Math.sqrt(3));
                 that.pen(true);
@@ -63,7 +132,7 @@ class TurtleGraph {
                 that.forward(eageLen * 15);
                 that.right(2 * pi / 3);
                 that.forward(eageLen * 15);
-    
+
                 that.pen(false);
                 that.init(0, 0, this.pi * 3 / 2);
                 that.forward(eageLen * 15 / 3 * Math.sqrt(3));
@@ -74,14 +143,14 @@ class TurtleGraph {
                 that.forward(eageLen * 15);
                 that.right(2 * pi / 3);
                 that.forward(eageLen * 15);
-    
-                let mid = 6;
-                let drawCicyle = (x, y) => {
-                    let radius = 0.03;
-                    let k = 5.7;
-                    let rx = (y % 2 == 1) ? eageLen * (k - x) : eageLen * (k - 0.5 - x);
-                    let ry = (eageLen - 0.01) * (19 - y);
-    
+
+                const mid = 6;
+                const drawCicyle = (x, y) => {
+                    const radius = 0.03;
+                    const k = 5.7;
+                    const rx = (y % 2 == 1) ? eageLen * (k - x) : eageLen * (k - 0.5 - x);
+                    const ry = (eageLen - 0.01) * (19 - y);
+
                     const swp = 20;
                     const OutSideTheta = 2 * pi / swp;
                     const Len = Math.sin(OutSideTheta / 2) * radius;
@@ -94,6 +163,7 @@ class TurtleGraph {
                         that.forward(Len);
                         that.left(OutSideTheta);
                     }
+                    that.pen(false);
                 }
                 for (let i = 1; i < 14; i += 1) {
                     drawCicyle(mid, i + 10);
@@ -106,7 +176,7 @@ class TurtleGraph {
                     }
                 }//build up-tri
                 for (let i = 1; i < 14; i++) {
-                    let ni = 18 - i;
+                    const ni = 18 - i;
                     if (ni > 13) {
                         drawCicyle(mid, ni + 10);
                     }
@@ -120,20 +190,20 @@ class TurtleGraph {
                         if (ni > 13 || (ni < 9 && k >= Math.floor(ni + 1) / 2)) {
                             {
                                 drawCicyle(mid - k, ni + 10);
-    
+
                             }
                         }
                     }//build up-three small tris
                 }
             };
-            console.log(that.arr2d)
+            //console.log(that.arr2d)
             drawEage();
         };
-        canvasN.addEventListener("click",this.clickCallback);
+        canvasN.addEventListener("click", this.clickCallback);
         //console.log("!" + canvasN);
-        this.canvas=canvasN;
+        this.canvas = canvasN;
         this.gl = canvasN.getContext("webgl");
-        let gl = this.gl;
+        const gl = this.gl;
         if (VSHADER_SOURCE === null) {
             return;
         }
@@ -181,7 +251,7 @@ class TurtleGraph {
         ceateProgram(VSHADER_SOURCE, FSHADER_SOURCE);
     }
     clickCallback;
-    
+
     init(x, y, theta) {
         if (theta > 2 * this.pi) {
             theta = theta % (2 * this.pi);
@@ -191,11 +261,11 @@ class TurtleGraph {
         this.theta = theta;
     }
     forward(distance, rgb = null) {
-        let xOffset = distance * Math.cos(this.theta);
-        let yOffset = distance * Math.sin(this.theta);
-        let nx = this.x + xOffset;
-        let ny = this.y + yOffset;
-        let drawLine = (x, y, nx, ny, gl) => {
+        const xOffset = distance * Math.cos(this.theta);
+        const yOffset = distance * Math.sin(this.theta);
+        const nx = this.x + xOffset;
+        const ny = this.y + yOffset;
+        const drawLine = (x, y, nx, ny, gl) => {
             this.gl.clear(gl.COLOR_BUFFER_BIT);
             const buffer = gl.createBuffer();
             const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
@@ -204,12 +274,12 @@ class TurtleGraph {
             gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.data), gl.STATIC_DRAW);
             const u_FragColor = gl.getUniformLocation(gl.program, "u_FragColor");
             gl.uniform4f(u_FragColor, 1.0, 0, 1, 1); // 绿色
-            let striteArray = [].concat.apply([],this.arr2d);
+            const striteArray = [].concat.apply([], this.arr2d);
             const u_width = gl.getUniformLocation(gl.program, 'u_Width');
             const u_Height = gl.getUniformLocation(gl.program, 'u_Height');
             gl.uniform1f(u_width, gl.drawingBufferWidth);
             gl.uniform1f(u_Height, gl.drawingBufferHeight);
-            let location = gl.getUniformLocation(gl.program, "u_BoxColor");
+            const location = gl.getUniformLocation(gl.program, "u_BoxColor");
             gl.uniform1iv(location, striteArray);
             //console.log(striteArray);
             gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
@@ -244,7 +314,7 @@ class TurtleGraph {
         }
         this.theta = ntheta;
     }
-    gl; x; y; theta; arr2d;canvas;
+    gl; x; y; theta; arr2d; canvas;
     pi = Math.PI;
     data = [];
 }
